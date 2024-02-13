aba1d970aeef1d9e8fb4a03440be964e4aa7acd5
JJHannah <zemjefferyzem@gmail.com>
2024-02-13 18:31:03 +0300

    changed the event listener

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ const form = document.querySelector("#form");
 const inputForm = document.querySelector(".inputText");
 const list = document.querySelector(".list");
 
-form.addEventListener("click", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   // console.log("click me");
   const inputValue = inputForm.value;
-  const newLi = document.createElement("li");
+  const newLi = document.createElement("LI");
   newLi.innerText = inputValue;
   list.append(newLi);
   inputForm.value = "";

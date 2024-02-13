52cc308fd04d1eac4f85afff94dc604c3a5c1759
JJHannah <zemjefferyzem@gmail.com>
2024-02-13 18:08:26 +0300

    add new elements

--- src/main.js
+++ src/main.js
@@ -7,4 +7,9 @@ const list = document.querySelector(".list");
 form.addEventListener("click", function (e) {
   e.preventDefault();
   // console.log("click me");
+  const inputValue = inputForm.value;
+  const newLi = document.createElement("li");
+  newLi.innerText = inputValue;
+  list.append(newLi);
+  inputForm.value = "";
 });

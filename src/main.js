2abed00c87f2490fa5b0b0baf186408678f8aca4
JJHannah <zemjefferyzem@gmail.com>
2024-02-13 17:34:35 +0300

    js files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-import './style.css'
+// import "./style.css";
+
+const form = document.querySelector("#form");
+const inputForm = document.querySelector(".inputText");
+const list = document.querySelector(".list");
+
+form.addEventListener("click", function (e) {
+  e.preventDefault();
+  // console.log("click me");
+});

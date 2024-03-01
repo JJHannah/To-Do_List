b86757a6c9834259605ec3f34b1453e5c24473e3
JJHannah <zemjefferyzem@gmail.com>
2024-03-01 18:58:10 +0300

    updated events file ,creating checkboxes

--- src/utils/events.js
+++ src/utils/events.js
@@ -8,7 +8,13 @@ form.addEventListener("submit", function (e) {
   const inputValue = inputForm.value;
   const newLi = document.createElement("LI");
   newLi.innerText = inputValue;
+
   list.append(newLi);
+  let checkBoxID = "checkbox" + inputForm;
+  const checkBox = document.createElement("INPUT");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("id", checkBoxID);
+  newLi.append(checkBox);
   inputForm.value = "";
 });
 

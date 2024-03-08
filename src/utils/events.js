14d09386f766fc00fb27aa31a1fe829798a5891a
JJHannah <zemjefferyzem@gmail.com>
2024-03-08 18:44:38 +0300

    revert js scripts fix bug

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,21 +1,23 @@
 const form = document.querySelector("#form");
-const inputForm = document.querySelector(".inputText");
-const list = document.querySelector(".list");
+const inputText = document.querySelector(".inputText");
+const ullist = document.querySelector(".list");
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("click", function (e) {
   e.preventDefault();
-  // console.log("click me");
-  const inputValue = inputForm.value;
-  const newLi = document.createElement("LI");
-  newLi.innerText = inputValue;
 
-  list.append(newLi);
-  let checkBoxID = "checkbox" + inputForm;
-  const checkBox = document.createElement("INPUT");
-  checkBox.setAttribute("type", "checkbox");
-  checkBox.setAttribute("id", checkBoxID);
-  newLi.append(checkBox);
-  inputForm.value = "";
+  const inputValue = inputText.value;
+
+  const newLi = document.createElement("LI");
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  newLi.appendChild(checkBox);
+
+  const textNode = document.createTextNode(inputValue);
+  ullist.appendChild(textNode);
+
+  ullist.appendChild(newLi);
+
+  inputText.value = "";
 });
 
 export default events;

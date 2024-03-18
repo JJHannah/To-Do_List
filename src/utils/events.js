37151f4e8a293651e4a45c4a3a2521eb258d7d60
JJHannah <zemjefferyzem@gmail.com>
2024-03-18 18:39:17 +0300

    fix render function to update  entries

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,8 +1,20 @@
 const eventsForm = () => {
   const inputElement = document.querySelector(".inputElement");
   const btnSave = document.querySelector(".btnSave");
+  const list = document.querySelector(".list");
   const todoList = [];
 
+  renderTodo();
+  function renderTodo() {
+    let todoListHTML = "";
+    for (let i = 0; i < todoList.length; i++) {
+      const todo = todoList[i];
+      const html = `<p>${todo}</p>`;
+      todoListHTML += html;
+
+      list.innerHTML = todoListHTML;
+    }
+  }
   btnSave.addEventListener("click", addTodo);
   function addTodo() {
     const inputValue = inputElement.value;
@@ -10,6 +22,8 @@ const eventsForm = () => {
     console.log(todoList);
 
     inputElement.value = "";
+
+    renderTodo();
   }
 };
 

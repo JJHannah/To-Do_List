193aaaf6e781ce01d5fc0d3b7c6b509a9adf94e5
JJHannah <zemjefferyzem@gmail.com>
2024-04-05 06:39:38 +0300

    fix bug on function save

--- src/utils/events.js
+++ src/utils/events.js
@@ -2,28 +2,14 @@ const eventsForm = () => {
   const inputElement = document.querySelector(".inputElement");
   const btnSave = document.querySelector(".btnSave");
   const list = document.querySelector(".list");
-  const todoList = [];
 
-  renderTodo();
-  function renderTodo() {
-    let todoListHTML = "";
-    for (let i = 0; i < todoList.length; i++) {
-      const todo = todoList[i];
-      const html = `<p>${todo}</p>`;
-      todoListHTML += html;
-
-      list.innerHTML = todoListHTML;
-    }
-  }
-  btnSave.addEventListener("click", addTodo);
-  function addTodo() {
-    const inputValue = inputElement.value;
-    todoList.push(inputValue);
-    console.log(todoList);
-
+  btnSave.addEventListener("click", save);
+  function save() {
+    const value = inputElement.value;
+    const newLi = document.createElement("LI");
+    newLi.innerText = value;
+    list.append(newLi);
     inputElement.value = "";
-
-    renderTodo();
   }
 };
 

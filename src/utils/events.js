9cd4e1104924607b42b7e932e674dab30a107cb2
Hannah <112068752+JJHannah@users.noreply.github.com>
2024-06-17 18:08:37 +0300

    Update events.js

--- src/utils/events.js
+++ src/utils/events.js
@@ -3,11 +3,11 @@ const eventsForm = () => {
   const btnSave = document.querySelector(".btnSave");
   const list = document.querySelector(".list");
   const todoList = localStorage.setItem('text') ? JSON.parse(localStorage.getItem('text')) :[];
-
+  
+ btnSave.addEventListener("click", save);
   
   function save() {
-    for (let i = 0; i < todoList.length; i++) {
-     btnSave.addEventListener("click", save);
+    for (let i = 0; i < todoList.length; i++) {    
     const value = inputElement.value;
     const newLi = document.createElement("LI");
     newLi.innerText = value;

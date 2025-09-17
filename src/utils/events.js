b2c99989626f7fb5641dcca218e7251d8ab4225e
JJHannah <zemjefferyzem@gmail.com>
2025-09-17 10:04:59 +0300

    Add localstorage on todolist array

--- src/utils/events.js
+++ src/utils/events.js
@@ -2,26 +2,22 @@ const eventsForm = () => {
   const inputElement = document.querySelector(".inputElement");
   const btnSave = document.querySelector(".btnSave");
   const list = document.querySelector(".list");
-  const todoList = localStorage.setItem('text') ? JSON.parse(localStorage.getItem('text') :[];
-  
- btnSave.addEventListener("click", saveItem);
-  
-  function saveItems() {
-    for (let i = 0; i < todoList.length; i++) {    
-    const value = inputElement.value;
-    const newLi = document.createElement("LI");
-    newLi.innerText = value;
-    list.append(new);
-    todoList.push(value);
-   inputElement.value = "";
-  }
-  }
-  function reset(){
- 
-    
-};
-  function edit(){
-        localStorage.clear();
-  }
 
+  btnSave.addEventListener("click", saveItem);
+
+  function saveItems() {
+    for (let i = 0; i < todoList.length; i++) {
+      const value = inputElement.value;
+      const newLi = document.createElement("LI");
+      newLi.innerText = value;
+      list.append(newLI);
+      todoList.push(value);
+      inputElement.value = "";
+    }
+  }
+  function reset() {}
+  function edit() {
+    localStorage.clear();
+  }
+};
 export default eventsForm;

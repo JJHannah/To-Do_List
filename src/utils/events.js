47049357b8c87f84ce0386967526b4493a7427a0
JJHannah <zemjefferyzem@gmail.com>
2025-10-20 14:39:00 +0300

    Add the edit option,also add checkboxes after completion of task

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,24 +1,37 @@
 const eventsForm = () => {
   const inputElement = document.querySelector(".inputElement");
   const btnSave = document.querySelector(".btnSave");
+  const btnReset = document.querySelector(".btnReset");
   const list = document.querySelector(".list");
-  const todoList = JSON.parse(localStorage.getItem("list")) || [];
+  let todoList = JSON.parse(localStorage.getItem("list")) || [];
 
   btnSave.addEventListener("click", saveItems);
+  addItems();
+  btnReset.addEventListener("click", reset);
 
   function saveItems() {
     const value = inputElement.value;
-    const newLi = document.createElement("LI");
-    newLi.innerText = value;
-    todoList.push(value);
-    list.append(newLi);
-    localStorage.setItem("list", JSON.stringify(todoList));
-    inputElement.value = "";
+    if (value) {
+      todoList.push(value);
+      localStorage.setItem("list", JSON.stringify(todoList));
+      inputElement.value = "";
+      addItems();
+    }
   }
 
   function addItems() {
     list.textContent = "";
-    todoList.forEach(function (item) {});
+
+    todoList.forEach((item) => {
+      const newLi = document.createElement("LI");
+      newLi.textContent = item;
+      list.appendChild(newLi);
+    });
+  }
+  function reset() {
+    todoList = [];
+    localStorage.clear();
+    list.textContent = " ";
   }
 };
 export default eventsForm;
